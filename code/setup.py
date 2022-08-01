6beba1498c8f00369ffe9e0a41dd586df90d09de
Faye-yufan <594744538@qq.com>
2022-08-01 12:13:59 -0700

    Create pypi package for conda skeleton

---
+++ code/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='analyticsdf',
+                 version='0.0.4.3',
+                 description='Analytic generation of datasets with specified statistical characteristics.',
+                 url='https://github.com/Faye-yufan/analytics-dataset',
+                 author='Fei',
+                 install_requires=['numpy','pandas','scipy'],
+                 author_email='',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
