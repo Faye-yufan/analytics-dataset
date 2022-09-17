cbcf799e9b8dce96a98b739a99cf3dd0fac661ed
Faye-yufan <yufanfei@usc.edu>
2022-09-16 21:26:44 -0700

    Update some tests with PyTest

--- code/tests/test_basic.py
+++ code/tests/test_basic.py
@@ -14,7 +14,7 @@ def test_specify_predictor_name():
     assert adf.predictor_matrix.columns.values.tolist() == ["xx1", "xx2", "xx3"]
 
 
-# Test case 1 - validate statistical properties of generated predictor matrix
+# validate statistical properties of generated predictor matrix
 def test_update_normal():
     ad = AnalyticsDataframe(10000, 3, ["xx1", "xx2", "xx3"], "yy")
     covariance_matrix = np.array([[1, -0.5, 0.3],
@@ -24,52 +24,35 @@ def test_update_normal():
                                 mean=[1, 2, 5],
                                 covariance_matrix=covariance_matrix)
     mean_list = list(ad.predictor_matrix.mean())
+    corr_list = ad.predictor_matrix.corr()
     assert [round(item, 1) for item in mean_list] == [1.0, 2.0, 5.0]
+    assert round(corr_list["xx1"][1], 1) == -0.5
+    assert round(corr_list["xx1"][2], 1) == 0.3
 
-    # print(test_matrix_1.corr())
-#           xx1       xx2       xx3
-# xx1  1.000000 -0.501385  0.300073
-# xx2 -0.501385  1.000000  0.203265
-# xx3  0.300073  0.203265  1.000000
+## Test handling of variable lists
+def test_predictor_normal_handling():
+    ad = AnalyticsDataframe(100, 3)
+    C = np.array([[1, -0.5, 0.3],
+                [-0.5, 1, 0.2],
+                [0.3, 0.2, 1]])
+    with pytest.raises(Exception):
+        ad.update_predictor_normal(predictor_name_list=["xx1", "xx2", "xx3"],
+                            mean=[1, 2, 5],
+                            covariance_matrix=C)
 
-##
-## Test case 2 - Test handling of variable lists
-##
+    # length of mean and length of predictor name list is different
+    with pytest.raises(ValueError):
+        ad.update_predictor_normal(predictor_name_list=["X1", "X2", "X3"],
+                                mean=[1, 2],
+                                covariance_matrix=C)
 
-# ad3 = ad.AnalyticsDataframe(100, 3)
-# C = np.array([[1, -0.5, 0.3],
-#               [-0.5, 1, 0.2],
-#               [0.3, 0.2, 1]])
-
-
-# # Should generate error due to mismatch of variable names
-# #
-# # ad3.update_predictor_normal(predictor_name_list=["xx1", "xx2", "xx3"],
-# #                             mean=[1, 2, 5],
-# #                             covariance_matrix=C)
-# # ...
-# # Exception: Please select the following predictors: ['X1', 'X2', 'X3']
-
-
-# # Should generate error due to mismatch of shape between covariance and mean
-# #
-# # C2 = np.array([[1, -0.5],
-# #               [-0.5, 1]])
-# # ad3.update_predictor_normal(predictor_name_list=["X1", "X2", "X3"],
-# #                             mean=[1, 2, 5],
-# #                             covariance_matrix=C2)
-# # ...
-# # ValueError: mean and cov must have same length
-
-
-# # Should generate error due to mismatch of shape between predictor and mean
-# #
-# # ad3.update_predictor_normal(predictor_name_list=["X1", "X2", "X3"],
-# #                             mean=[1, 2],
-# #                             covariance_matrix=C)
-# # ...
-# # ValueError: predictor and mean must have same length
-
+    # shape of covariance matrix is unmatched with length of predictor_name_list
+    C = np.array([[1, -0.5],
+                [-0.5, 1]])
+    with pytest.raises(ValueError):
+        ad.update_predictor_normal(predictor_name_list=["X1", "X2", "X3"],
+                                    mean=[1, 2, 5],
+                                    covariance_matrix=C)
 
 # # Test case: update_predictor_beta
 # #
